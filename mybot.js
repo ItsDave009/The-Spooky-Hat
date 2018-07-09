8ad5fa4acfa099b4c69d15872878c842071d7c28
ItsDave009 <40859203+ItsDave009@users.noreply.github.com>
2018-07-09 21:09:09 +0530

    Create mybot.js

---
+++ mybot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("Let's Go Dave!!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("status")) {
+    message.channel.send("going offline now.. Bye !!");
+  }
+});
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong");
+  }
+});
+client.login("NDY1OTAzNDYwOTIwMDY2MDQ4.DiUR8w.nLsaNVWbB7qcxrkNukKPtRcHmt0");
