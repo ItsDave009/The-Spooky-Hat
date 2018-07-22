f24c3d6623047cbf66a7bd1d7fee3cd00293d695
ItsDave009 <40859203+ItsDave009@users.noreply.github.com>
2018-07-22 18:40:18 +0530

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -10,16 +10,5 @@ client.on("message", (message) => {
     message.channel.send("All bots are functional");
   }
 });
-client.on("message", (message) => {
-  if (message.content.startsWith("!duration")) {
-    message.channel.send("00");
-  }
-  });
-
-client.on("message", (message) => {
-  if (message.content.startsWith("/help")) {
-    message.channel.send("No commands. My work is only to tell the status of the bot.. If you want to check type **'!status'**");
-  }
-});
               
 client.login("NDY1OTAzNDYwOTIwMDY2MDQ4.DiUR8w.nLsaNVWbB7qcxrkNukKPtRcHmt0");
